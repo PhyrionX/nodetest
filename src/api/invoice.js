16edf3339ad6b7d415ffc0ebbcaa1b0a1e15c5c6
gabasrub <ruben.gabas@adidas.com>
2020-04-05 18:00:26 +0200

    third and second  done

--- src/api/invoice.js
+++ src/api/invoice.js
@@ -1,5 +1,5 @@
 module.exports = (app) => {
-  app.get('/invoice/:invoiceId', async(req, res) => {
+  app.get('/invoice/:invoiceId/advanced', async(req, res) => {
     const invoiceService = req.container.resolve('invoiceService');
     const { invoiceId } = req.params;
     const { authorization } = req.headers;
@@ -47,6 +47,44 @@ module.exports = (app) => {
       } catch (err) {
         res.status(400).send(err);
       }
+    } else {
+      res.status(400).send('Error in parameters or authorization');
+    }
+  });
+
+  app.get('/invoice/:invoiceId', async(req, res) => {
+    const invoiceService = req.container.resolve('invoiceService');
+    const { invoiceId } = req.params;
+    const { authorization } = req.headers;
+
+    if(invoiceId && authorization) { 
+      invoiceService.getInvoice({ invoiceId, authorization })
+        .then(({data: {data}}) => {
+          return invoiceService.sendInvoice({
+            invoice: {
+              ...data,
+              customer: data.client,
+              date: new Date().toString(),
+              invoice: invoiceId,
+              customerName: '',
+              total: data.lineItems.reduce((acc, curr) => acc + (curr.quantity * curr.unitPrice), 0),
+              lineItems: data.lineItems.map((lineProduct) => ({
+                ...lineProduct,
+                price: lineProduct.quantity * lineProduct.unitPrice
+              }))
+            },
+            authorization
+          })
+        })
+        .then(({ data }) => {
+          console.log(data);
+          
+          res.status(200).send(data)
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(400).send(err);
+        });
     } else {
       res.status(400).send('Error in parameters or authorization');
     }
